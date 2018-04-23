38a38a6e4ba8d1071a2fa509787cd2f35f761c78
foxsin10 <fox_we10@163.com>
2018-04-23 17:36:10 +0800

    update some script

--- MAC/toolburst.sh
+++ MAC/toolburst.sh
@@ -1,3 +1,10 @@
 
-## homebrew install
+## homebrew install 可能因为端口问题
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+## ruby update
+gem source --remove https://rubygems.org/
+gem sources -a https://gems.ruby-china.org/
+sudo gem update --system
+sudo gem install -n /usr/local/bin cocoapods
+pod setup
